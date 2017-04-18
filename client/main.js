8480dce5a13a27ab526c402c26a11bb18f872079
Dexter Shepherd <dexter@kadenze.com>
2017-04-18 16:48:50 -0700

    making render horizontal

--- client/main.js
+++ client/main.js
@@ -11,26 +11,28 @@ const renderer = new P5((p) => {
     const cellW = w / grid.width;
     const cellH = h / grid.height;
 
-    p.push();
-    p.translate(x, y);
     for (let i = 0; i < grid.width; i += 1) {
       for (let j = 0; j < grid.height; j += 1) {
         if (cells[i][j] === 1) {
-          p.rect(i * cellW, j * cellH, cellW, cellH);
+          p.rect(x + i * cellW, y + j * cellH, cellW, cellH);
         }
       }
     }
-    p.pop();
   }
 
   function render(input) {
     p.background(250);
     p.noStroke();
     p.fill(200);
+    p.push();
+    p.translate(100, 100);
     input.rules.forEach((rule, i) => {
       const grid = new Grid(rule, 300, 50);
-      drawCells(100, (i + 1) * 70, 600, 70, grid);
+      const w = p.map(input.words[i].length, 0, input.length, 0, 600);
+      drawCells(0, 0, w, 100, grid);
+      p.translate(w, 0);
     });
+    p.pop();
   }
 
   p.setup = () => {
